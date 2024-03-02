52b1993e21825f9580384402cfd43d6d5d8a6495
Cesar Ortega <cesarluis4056@gmail.com>
2024-03-02 16:35:00 -0500

    Continuando con el curso de Jon Mircha

--- spa/app/App.js
+++ spa/app/App.js
@@ -1,5 +1,17 @@
+import { ajax } from "./helpers/ajax.js";
+import api from "./helpers/wp_api.js";
+
 export function App() {
   document.getElementById(
     "root"
   ).innerHTML = `<h1> Bienvenidos a mi 1er SPA con Vanilla Js</h1>`;
 }
+
+console.log(api);
+
+ajax({
+  url: api.POSTS,
+  cbSuccess: (posts) => {
+    console.log(posts);
+  },
+});
